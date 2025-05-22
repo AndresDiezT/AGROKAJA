14d5674dcc9c0d612d799c213d5ef8bd1d33d732
Andres <andresdieztuberquia@gmail.com>
2025-05-22 08:37:23 -0500

    Initial structure for backend

---
+++ MicroServices/src/Services/RoleService.Api/Data/RolesDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using RoleService.Api.Models;
+
+namespace RoleService.Api.Data
+{
+    public class RolesDbContext : DbContext
+    {
+        public RolesDbContext(DbContextOptions<RolesDbContext> options) : base(options) { }
+
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>().ToTable("Roles");
+        }
+    }
+}
---
+++ MicroServices/src/Services/RoleService.Api/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿namespace RoleService.Api.Models
+{
+    public class Role
+    {
+        public int IdRole { get; set; }
+        public string NameRole { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool isActive { get; set; }
+        public DateTime? DeactivatedAt { get; set; }
+    }
+}
